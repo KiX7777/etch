fb0e566c572afccd74e3a02c3115a2a0894165f5
KiX7777 <kristianboi202@gmail.com>
2022-11-25 11:27:07 +0100

    finished basic functions

--- script.js
+++ script.js
@@ -4,9 +4,39 @@
 const container = document.querySelector('.container');
 container.style.display = 'grid';
 const celije = document.querySelectorAll('.cell');
+const colorpick = document.querySelector('#colorpick');
+console.log(colorpick.value);
+
+for (let i = 0; i < 16 * 16; i++) {
+  const cell = document.createElement('div');
+  cell.className = 'cell';
+  cell.id = 'cell';
+  cell.style.border = '0.5px solid orange';
+  container.appendChild(cell);
+  container.style.gridTemplateColumns = `repeat(16, 1fr)`;
+  // cell.style.transition = '0.2s all';
+  container.style.transition = '0.2s all';
+
+  document.querySelectorAll('#cell').forEach((cell) => {
+    cell.addEventListener('mouseover', function () {
+      cell.style.backgroundColor = '#b7ff4c';
+    });
+  });
+}
+// cell.style.padding = '1rem';
+// container.style.gridTemplateColumns = `repeat(16, 1fr)`;
+
+//očisti grid
+
+const clearGrid = function () {
+  document.querySelectorAll('#cell').forEach((cell) => {
+    cell.remove();
+  });
+};
 
 //napravi grid
 const makeGrid = function () {
+  clearGrid();
   let boxNum = prompt('Koliki grid?');
   for (let i = 0; i < boxNum * boxNum; i++) {
     if (boxNum > 50) {
@@ -16,23 +46,21 @@ const makeGrid = function () {
       const cell = document.createElement('div');
       cell.className = 'cell';
       cell.id = 'cell';
-      // cell.style.padding = '1rem';
+      cell.style.border = '0.5px solid #333';
+      cell.style.transition = '0.1s all';
       container.appendChild(cell);
+
       container.style.gridTemplateColumns = `repeat(${boxNum}, 1fr)`;
+      hoverColor();
     }
   }
 };
 
-makeGrid();
-
-celije.forEach((element) => {
-  console.log(element);
-});
-
-document.querySelectorAll('#cell').forEach((cell) => {
-  cell.addEventListener('mouseover', function () {
-    cell.style.backgroundColor = '#b7ff4c';
-  });
-});
-
 //oboji ćeliju prilikom prelaska preko nje
+const hoverColor = function () {
+  document.querySelectorAll('#cell').forEach((cell) => {
+    cell.addEventListener('mouseover', function () {
+      cell.style.backgroundColor = `${colorpick.value}`;
+    });
+  });
+};
